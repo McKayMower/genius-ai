2b3f807e6fed16ca25c26056391032149c03e278
McKay Mower <mmower777@gmail.com>
2023-08-12 22:57:58 -0600

    video api route

---
+++ app/api/video/route.ts
@@ -0,0 +1,36 @@
+import { musicFormSchema } from "@/app/(dashboard)/(routes)/music/constants";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY!,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = musicFormSchema.parse(body);
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!prompt) return new NextResponse("Prompt is required", { status: 400 });
+
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
+
+    return NextResponse.json(response);
+  } catch (error: any) {
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
