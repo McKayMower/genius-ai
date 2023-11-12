b099a47b2173ddd085dfaf4899c15d7898e61bc5
McKay <mmower777@gmail.com>
2023-11-11 17:03:31 -0700

    db migration

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -33,11 +33,21 @@ export async function POST(req: Request) {
     if (!freeTrial && !isPro)
       return new NextResponse("Free trial has expired", { status: 403 });
 
+    console.log("checking trial4");
+
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: parseInt(amount, 10),
+      size: resolution
+    })
+
+    console.log('checking trial 5')
     const response = await openai.createImage({
       prompt: prompt,
       n: parseInt(amount, 10),
       size: resolution,
     });
+    console.log('checking trial 6')
 
     // increase limit by one since response was generated
     if (!isPro) await increaseApiLimit();
